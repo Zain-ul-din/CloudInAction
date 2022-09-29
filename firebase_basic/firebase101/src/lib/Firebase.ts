211f6556a5dd64e512bd90862d3a9424732b7f96
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-09-29 14:28:12 +0500

    firebase exports

--- firebase_basic/firebase101/src/lib/Firebase.ts
+++ firebase_basic/firebase101/src/lib/Firebase.ts
@@ -7,10 +7,12 @@ import { getFirestore } from 'firebase/firestore'
 import  { config } from '../config/Config'
 
 import type { FirebaseApp } from 'firebase/app'
+import type { Auth } from 'firebase/auth'
 
 export default function FirebaseInitialize ()
 {
     
    const app: FirebaseApp = initializeApp (config.firebaseConfig)
-    
+   const firebaseAuth: Auth = getAuth (app)
+   
 }

83a5d3f10c484abda1980e6b5ec1e57ea1e13218
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-10-03 14:21:24 +0500

    firebase exports

--- firebase_basic/firebase101/src/lib/Firebase.ts
+++ firebase_basic/firebase101/src/lib/Firebase.ts
@@ -4,4 +4,9 @@
 import { initializeApp, getApp } from 'firebase/app'
 import {  getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import  { config } from '../config/Config'
 
+export default function ()
+{
+
+}

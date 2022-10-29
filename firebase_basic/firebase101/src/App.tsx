df5e9e45cc1523d049a0b20ae96a6f0ef2f4f0f2
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-10-29 21:16:42 +0500

    cloud

--- firebase_basic/firebase101/src/App.tsx
+++ firebase_basic/firebase101/src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 // Firebase imports
 import { config } from './config/Config'
 import { FirebaseApp, initializeApp } from 'firebase/app'
-import { Firestore, getFirestore } from 'firebase/firestore'
+import 
+{ 
+    Firestore,
+    getFirestore, // acess firestore
+    collection,  // collection access
+    onSnapshot, // on data change
+    doc, // document ref
+    setDoc, // write to document
+    CollectionReference,
+    DocumentData,
+    QuerySnapshot,
+    QueryDocumentSnapshot,
+    DocumentReference,
+    DocumentSnapshot
+} from 'firebase/firestore'
+
 
 // init App
 const firebaseApp:FirebaseApp = initializeApp (config.firebaseConfig)
@@ -11,12 +26,52 @@ const firebaseApp:FirebaseApp = initializeApp (config.firebaseConfig)
 // firebase store acess
 const firebaseStore:Firestore = getFirestore (firebaseApp)
 
+// reference to collection
+const chatRoomCol_Ref: CollectionReference <DocumentData> = collection (firebaseStore, 'chat room')
 
+// document Ref
+const document_Ref: DocumentReference <DocumentData> = doc (chatRoomCol_Ref, 'nPtxYe3Uxff5vaJov3jZ') 
+
+// set doc
+function SetDoc (data: object): void 
+{
+    setDoc (document_Ref, data)
+}
 
 export default function App () : React.ReactElement {
+
+    const [messages, setMessages] = useState <DocumentData[]> ([])
+
+    useEffect (()=>{
+        
+        ///
+        /// On SpanShot Collection
+        ///
+
+        // onSnapshot (chatRoomCol_Ref, (snapShot: QuerySnapshot <DocumentData>) => {
+        //     const newData = snapShot.docs.map ( (val: QueryDocumentSnapshot <DocumentData>) => {
+        //         return val.data ()
+        //     });
+
+        //     setMessages (newData)
+        // })
+
+        ///
+        /// On SpanShot Query
+        ///
+
+        onSnapshot  (document_Ref, (snapShot: DocumentSnapshot <DocumentData>) => {
+            console.log (snapShot.data ())
+        })
+
+    }, [])
+
+
     return (
         <>
             <h1>FireBase</h1>
+            
         </>
     )
 }
+
